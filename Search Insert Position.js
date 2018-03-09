276a604285b1454a8496218dd4e76362a065916e
Kevin Yeh <revinyeh@gmail.com>
2018-03-09 11:20:51 -0800

    /**

--- Search Insert Position.js
+++ Search Insert Position.js
@@ -4,6 +4,35 @@ return the index where it would be if it were inserted in order.
 You may assume no duplicates in the array.
 */
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ 
 var searchInsert = function(nums, target) {
-    
+   for(var i = 0; i < nums.length; i++) {
+   		console.log(i,nums[i],target)
+   		if(target <= nums[i]) return i
+   	}
+	return nums.length
+};
+
+var searchInsert = function(nums, target) {
+    var l = 0;
+    var u = nums.length-1;
+    var mid;
+
+    while(l <= u) {
+        mid = parseInt((u+l)/2);
+        if(target === nums[mid]) {
+            return mid;
+        } else if(target < nums[mid]) {
+          u = mid-1;
+        } else {
+          l = mid+1;
+        }
+    }
+
+    return l;
 };
