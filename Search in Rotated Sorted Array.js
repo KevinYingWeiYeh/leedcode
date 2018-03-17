24ae3c891be4ae76e6f1108d85003c22aab100f7
Kevin Yeh <revinyeh@gmail.com>
2018-03-16 17:03:36 -0700

    A valid Sudoku start

--- Search in Rotated Sorted Array.js
+++ Search in Rotated Sorted Array.js
@@ -15,5 +15,29 @@ You may assume no duplicate exists in the array.
  * @return {number}
  */
 var search = function(nums, target) {
-  return nums.indexOf(target)  
+  console.log(nums)
+  while(nums[0] > nums[nums.length-1]){
+  	nums.push(nums.shift())
+  }
+  var first = 0;
+  var end = nums.length;
+  console.log(nums)
+  while(Math.round(nums[end-first/2]) !== target) {
+  	console.log(first,end,end-first/2)
+  	if(nums[end-first/2] > target) {
+  		end = Math.ceil(end-first/2)
+  	} else {
+  		first = Math.ceil(end-first/2)
+  	}
+  	
+  } 
+  return Math.round(end-first/2)
 };
+
+// console.log(search([4,5,6,7,0,1,2],5))
+
+var a = new Object({type:'student'})}
+function a(s) {
+	s.name = 'john',
+	s.age = 15
+} 
